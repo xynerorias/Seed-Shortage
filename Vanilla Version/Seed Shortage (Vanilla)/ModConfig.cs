84ab4b65f2649d639e83a9aee58b62b6bd0331df
xynerorias <43299926+xynerorias@users.noreply.github.com>
2020-03-20 23:15:32 +0100

    Add files via upload

--- Vanilla Version/Seed Shortage (Vanilla)/ModConfig.cs
+++ Vanilla Version/Seed Shortage (Vanilla)/ModConfig.cs
@@ -7,7 +7,7 @@
         public bool KrobusEnabled { get; set; } = true;
         public bool JojaEnabled { get; set; } = true;
         public bool TravellingEnabled { get; set; } = true;
-        public bool TravellingNO_Coffee { get; set; } = false;
+        public bool TravellingNO_Coffee { get; set; } = true;
         public bool TravellingNO_RareSeed { get; set; } = true;
         public bool PierreNO_SringFestivalSeeds { get; set; } = false;
         public bool SandyNO_CactusSeeds { get; set; } = false;
