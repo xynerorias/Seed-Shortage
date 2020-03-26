419f0fc0832cb63b1508b7bdb34b024faedd7a92
xynerorias <43299926+xynerorias@users.noreply.github.com>
2020-03-26 04:00:25 +0100

    Add files via upload

---
+++ test/ID.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeedShortageJA
+{
+    public class ID
+    {
+        public static Dictionary<string, int> Dict = new Dictionary<string,int>();
+
+        public void AddToDic()
+        {
+            Dict.Add("Hops Starter", 302);
+            Dict.Add("Rare Seed", 347);
+            Dict.Add("Fairy Seeds", 425);
+            Dict.Add("Tulip Bulb", 427);
+            Dict.Add("Jazz Seeds", 429);
+            Dict.Add("Sunflower Seeds", 431);
+            Dict.Add("Sunflower Seeds", 433);
+            Dict.Add("Poppy Seeds", 453);
+            Dict.Add("Spangle Seeds", 455);
+            Dict.Add("Parsnip Seeds", 472);
+            Dict.Add("Bean Starter", 473);
+            Dict.Add("Cauliflower Seeds", 474);
+            Dict.Add("Potato Seeds", 475);
+            Dict.Add("Garlic Seeds", 476);
+            Dict.Add("Kale Seeds", 477);
+            Dict.Add("Rhubarb Seeds", 478);
+            Dict.Add("Melon Seeds", 479);
+            Dict.Add("Tomato Seeds", 480);
+            Dict.Add("Blueberry Seeds", 481);
+            Dict.Add("Pepper Seeds", 482);
+            Dict.Add("Wheat Seeds", 483);
+            Dict.Add("Radish Seeds", 484);
+            Dict.Add("Red Cabbage Seeds", 485);
+            Dict.Add("Starfruit Seeds", 486);
+            Dict.Add("Corn Seeds", 487);
+            Dict.Add("Eggplant Seeds", 488);
+            Dict.Add("Artichoke Seeds", 489);
+            Dict.Add("Pumpkin Seeds", 490);
+            Dict.Add("Bok Choy Seeds", 491);
+            Dict.Add("Yam Seeds", 492);
+            Dict.Add("Cranberry Seeds", 493);
+            Dict.Add("Beet Seeds", 494);
+            Dict.Add("Spring Seeds", 495);
+            Dict.Add("Summer Seeds", 496);
+            Dict.Add("Fall Seeds", 497);
+            Dict.Add("Winter Seeds", 498);
+            Dict.Add("Ancient Seeds", 499);
+            Dict.Add("Strawberry Seeds", 745);
+            Dict.Add("Mixed Seeds", 770);
+            Dict.Add("Cactus Seeds", 802);
+        }
+    }
+}
---
+++ test/ModConfig.cs
@@ -0,0 +1,19 @@
+﻿namespace SeedShortageJA
+{
+    public class ModConfig
+    {
+        public bool PierreEnabled { get; set; } = true;
+        public bool SandyEnabled { get; set; } = true;
+        public bool MarnieEnabled { get; set; } = true;
+        public bool HarveyEnabled { get; set; } = false;
+        public bool ClintEnabled { get; set; } = false;
+        public bool KrobusEnabled { get; set; } = true;
+        public bool JojaEnabled { get; set; } = true;
+        public bool TravellingEnabled { get; set; } = true;
+        public string[] Exceptions { get; set; } =
+        {
+            "Parsnip Seeds",
+            "Beefvine Seeds"
+        };
+    }
+}
